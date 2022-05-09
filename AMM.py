8c5554f1e8dbab3e629c470484b0017ff96a1f0e
AMM <88286248+AishatuMM@users.noreply.github.com>
2022-05-09 20:50:54 +0800

    Add files via upload

---
+++ AMM.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+name = "Aishatu MMalami"
+email = "ummiamal@gmail.com"
+slackusername = "@AMM"
+interest = "metagenomics"
+
+print("{}, {}, {}, {}.format(name, email, slackusername, interest")
