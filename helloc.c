602544a520de4742da2019d4f88a7103df597f85
Muhammad Mustapha Ibrahim <104961599+Mimuhammad01@users.noreply.github.com>
2022-05-11 06:01:41 -0400

    Add files via upload

---
+++ helloc.c
@@ -0,0 +1,10 @@
+#iclude<stdio.h>
+int main()
+{
+printf("Muhammad Mustapha Ibrahim");
+printf("Mimuhammad01@gmail.com");
+printf(@ Muhammad Mustapha Ibrahim");
+printf("Metabolomics");
+printf("C++ Programming");
+retun 0;
+}
